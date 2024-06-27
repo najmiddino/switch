26b5097d97264e89e9cd14d4749ebff639124e25
najmiddino <164999098+najmiddino@users.noreply.github.com>
2024-06-27 14:38:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+let n = +prompt('birdan 7 gacha son kiriting')
+
+switch (n) {
+    case 1:
+        alert (`dushanba`)
+        break;
+    case 2:
+        alert (`seshanba`)
+        break;
+    case 3:
+        alert (`chorshanba`)
+        break;
+    case 4:
+        alert (`payshanba`)
+        break;
+    case 5:
+        alert (`juma`)
+        break;
+    case 6:
+        alert (`shanba`)
+        break;
+    case 7:
+            alert (`yakshanba`)
+        break;
+    default:
+        alert ('7 gacha bolgan son kiriting')
+        break;
+}
